e92aee9acb7100da17cf92ceff3552bd063bfda8
laxesta <saracyan78@hotmail.com>
2022-05-24 15:12:38 +0200

    Migracion. Login basico funcionando, falta logout.//Migration performed. Basic login working fine, logout still missing

---
+++ RedesCookies_API/Migrations/20220523223301_Inicial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RedesCookies_API.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Apelidos = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ExternalLoginNome = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
--- SocialCookies.Bwasm/Program.cs
+++ SocialCookies.Bwasm/Program.cs
@@ -1,3 +1,5 @@
+using Blazored.LocalStorage;
+
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -41,6 +43,7 @@ builder.Services.AddAuthorizationCore();
 //we register our customised AuthStateProvider
 builder.Services.AddScoped<AuthenticationStateProvider, PersonalAuthStateProvider>();
 builder.Services.AddScoped<CookieXestor>();
+builder.Services.AddBlazoredLocalStorage();
 
 
 await builder.Build().RunAsync();
